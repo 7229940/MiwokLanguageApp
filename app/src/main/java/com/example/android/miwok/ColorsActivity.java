2143604befab46e3a0c32c0c2f33670be6d79c04
goraVashistha <gourav.vashishtha.gourav@gmail.com>
2018-06-13 11:33:22 +0530

    updated code

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.miwok;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,12 +13,51 @@ import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
-    private MediaPlayer mediaPlayer;
+    private MediaPlayer mMediaPlayer;
+
+    AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener =
+            new AudioManager.OnAudioFocusChangeListener() {
+                public void onAudioFocusChange(int focusChange) {
+                    if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+
+                        releaseMediaPlayer();
+                    }
+                    else if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                        // Pause playback
+                        mMediaPlayer.pause();
+                        mMediaPlayer.seekTo(0);
+                    }  else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                        // Your app has been granted audio focus again
+                        // Raise volume to normal, restart playback if necessary
+                        mMediaPlayer.start();
+                    }
+                }
+            };
+
+    // this is the oncomletionListener for an audio file
+    // we have created it globally so that it would not be needed to make a new object for each click.
+
+    private MediaPlayer.OnCompletionListener mOnCompletionListener= new MediaPlayer.OnCompletionListener() {
+
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            releaseMediaPlayer();
+        }
+
+    };
+
+    //Handles audio focus when playing a sound file.
+    private AudioManager mAudioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mAudioManager=(AudioManager)getSystemService(getApplicationContext().AUDIO_SERVICE);
+
         final ArrayList<Word> words = new ArrayList<Word>();
         //word.add("one");
 //        word.add("two");
@@ -59,11 +99,64 @@ public class ColorsActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Word word=words.get(position);
-                Toast.makeText(getApplicationContext(),"sound is going to be produced",Toast.LENGTH_SHORT).show();
-                mediaPlayer=MediaPlayer.create(getApplicationContext(),word.getAudioResourceId());
-                mediaPlayer.start();
+                //  Toast.makeText(getApplicationContext(),"sound is going to be produced",Toast.LENGTH_SHORT).show();
+
+                //release media player if it currently exitsts because we are about to play a different sound file.
+                releaseMediaPlayer();
+
+                // Request audio focus for playback
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        // Use the music stream.
+                        AudioManager.STREAM_MUSIC,
+                        // Request temporary focus because we want to play an audio file of one or two second long.
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+
+                    // we have audio focus now
+                    mMediaPlayer = MediaPlayer.create(getApplicationContext(), word.getAudioResourceId());
+                    mMediaPlayer.start();
+
+                    /**
+                     * setup a listener on the media player so that we can stop or release the
+                     * media player once the sounds has finished playing.
+                     *
+                     * Clean up the media player by releasing its resources on completion the audio file.
+                     */
+
+                    mMediaPlayer.setOnCompletionListener(mOnCompletionListener);
+                }
             }
         });
 
     }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //when the activity is stopped, release the media player resources
+        //becuse we don't want to play any sound any more.
+        releaseMediaPlayer();
+    }
+
 }
